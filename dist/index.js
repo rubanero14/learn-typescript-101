479f631eac94acbd267cbe6c34727722ed9dff87
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2023-05-07 21:50:36 +0800

    Create Data Type, Function Type, Object Type and Interface

--- dist/index.js
+++ dist/index.js
@@ -7,4 +7,14 @@ var message = "hello";
 message = "world";
 // Function Type
 var myNameIs = function (name) { return "Your name is ".concat(name, "!"); };
-output.textContent = myNameIs("Rajaruban");
+var user = {
+    name: "Raj",
+};
+var user2 = {
+    name: "Sri",
+    age: 4,
+    greet: function () {
+        return "Hi ".concat(this.name, "!");
+    },
+};
+output.textContent = user2.greet();
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 const title: HTMLTitleElement = document.querySelector("title");
 title.textContent = "Learn Typescript";
 
-const output = document.querySelector(".ts-output");
+const output: any = document.querySelector(".ts-output");
 
 // Data Type
 let message: string = "hello";
@@ -11,4 +11,23 @@ message = "world";
 // Function Type
 const myNameIs = (name: string): string => `Your name is ${name}!`;
 
-output.textContent = myNameIs("Rajaruban");
+// Object Type
+// Defining Custom Type for Object and its contents using Interface
+interface User {
+  name: string;
+  age?: number; // adding ? after data type in interface indicates tells typescript to omit if this data type/parameter is not declared or not exist
+  greet?(): string; // functions also can be defined the return value's data type
+}
+const user: User = {
+  name: "Raj",
+};
+
+const user2: User = {
+  name: "Sri",
+  age: 4,
+  greet() {
+    return `Hi ${this.name}!`;
+  },
+};
+
+output.textContent = user2.greet();
